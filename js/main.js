f44ac0d24b0dd478079450ffc6dd9fc73eb7c953
tamga05 <tamga05@gmail.com>
2023-04-22 09:14:00 +0300

    added slick-slider and background image header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
+$(function () {
+
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
+        vertical: true
+    });
 
 });
