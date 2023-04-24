6c7929c0e8203b58775ec97e22d1c9d4c8059ec1
tamga05 <tamga05@gmail.com>
2023-04-24 17:16:10 +0300

    completed markup and styles header

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ $(function () {
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
-        vertical: true
+        vertical: true,
+        autoplay: true
     });
 
 });
