76fa47ca046ceeebb409c5c053d6f37461e1ac0a
tamga05 <tamga05@gmail.com>
2023-04-26 00:47:33 +0300

    added markup and styles section product

--- js/main.js
+++ js/main.js
@@ -8,5 +8,28 @@ $(function () {
         autoplay: true
     });
 
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        // arrows: false,
+        // fade: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        autoplay: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt="product-prev"></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt="product-next"></button>'
+    });
+    $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        // dots: true,
+        // centerMode: true,
+        // focusOnSelect: true
+        // fade: true,
+        arrows: false
+    });
+
 });
 
